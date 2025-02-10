b084196dc4f66febb51f6d830e8bb13f49cddce8
biancaEnicole <trabalholourdaobinibini@gmail.com>
2025-02-10 10:41:17 -0300

    01 - criando a página index.html  e o arquivo inicial de JS

---
+++ js/base.js
@@ -0,0 +1,23 @@
+// exibir o console
+// console só existe no navegador
+console.log('Olá sou o console');
+console.log('eu exibo conteúdo');
+
+// variáveis
+let rmAluno =666
+let nomeAluno = 'rivaldo jesus'
+let turmaAluno = '3TI'
+const  ESCOLA_ALUNO = 'Lourdão'
+
+console.log(rmAluno, nomeAluno)
+console.log(turmaAluno, ESCOLA_ALUNO)
+
+console.log('Nome do aluno:' + nomeAluno + ' RM do aluno:' +rmAluno)
+
+// template string
+console.log(`Nome do aluno ${nomeAluno} Rm do aluno ${rmAluno}`)
+
+turmaAluno ='3A'
+console.log(`Turma do aluno: ${turmaAluno}`);
+
+
